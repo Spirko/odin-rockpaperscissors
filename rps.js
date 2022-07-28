89f77ccd5c3b1dffb6351c25a2df7f250573ee84
Spirko <spirko@gmail.com>
2022-07-27 23:58:37 -0500

    Implement playRound

--- rps.js
+++ rps.js
@@ -2,8 +2,31 @@ console.log("RPS starting.");
 
 function getComputerChoice() {
   switch (Math.floor(Math.random()*3)) {
-    case 0: return 'Rock';
-    case 1: return 'Paper';
-    case 2: return 'Scissors';
+    case 0: return 'rock';
+    case 1: return 'paper';
+    case 2: return 'scissors';
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  switch (playerSelection.toLowerCase()) {
+    case 'rock':
+      switch (computerSelection) {
+        case 'rock': return 'Tie, both rock';
+        case 'paper': return 'Computer, paper covers rock';
+        case 'scissors': return 'Player, rock crushos scissors';
+      }
+    case 'paper':
+      switch (computerSelection) {
+        case 'rock': return 'Player, paper covers rock';
+        case 'paper': return 'Tie, both paper';
+        case 'scissors': return 'Computer, scissors cut paper';
+      }
+    case 'scissors':
+      switch (computerSelection) {
+        case 'rock': return 'Computer, rock crushes scissors';
+        case 'paper': return 'Player, scissors cut paper';
+        case 'scissors': return 'Tie, both scissors';
+      }
   }
 }
